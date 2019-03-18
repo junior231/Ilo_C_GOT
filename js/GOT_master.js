11b9af4318e4d35e1def511cbc165a670eabe3ed
Ilo <c_ilo@tss-hr420-dm21.fc.ca>
2019-03-18 15:25:33 -0400

    phase 3 done

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,12 +3,27 @@
 
 
 // Grab the sheildsat the bottom of the page
-	const shields = document.querySelectorAll('.sigil-container'),
-		  lightBox = document.querySelector('.lightbox'),
+	const shields = document.querySelectorAll('.sigil-container');
+		  lightBox = document.querySelector('.lightbox');
 		  video = document.querySelector('video');
+		  closeLB = document.querySelector('.lightbox-close');
 
 	function openLightbox() {
+		// grab the right video source
+		// debugger;
+		// get the lowercase house name from the class list
+		let targetHouse = this.className.split(" ")[1];
+		// console.log(targetHouse);
+		
+		// make sure the names match - nees to be uppercase
+		// stark becomes Stark -> first make a capital S, then add tark (or any house name)
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
+
 		lightBox.classList.add('lightbox-on');
+		video.load();
 		video.play();
 	}
 	function closeLightbox() {
@@ -22,4 +37,5 @@
 	shields.forEach(sigil => sigil.addEventListener('click', openLightbox));
 
 	video.addEventListener('ended', closeLightbox);
+	closeLB.addEventListener('click', closeLightbox);
 })();
